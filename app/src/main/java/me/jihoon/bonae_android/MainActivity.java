3fdcbec1298e4f141dd2cf98cc4304418daf60f9
Jihoon Ko <jihoonko@kaist.ac.kr>
2017-07-13 17:19:34 +0900

    add facebook sdk

--- app/src/main/java/me/jihoon/bonae_android/MainActivity.java
+++ app/src/main/java/me/jihoon/bonae_android/MainActivity.java
@@ -3,11 +3,16 @@ package me.jihoon.bonae_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
     }
 }
