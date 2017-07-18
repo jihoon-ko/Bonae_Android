ebb2c7c90a63a6625e2acf8abcce01a99be293f4
cafelatte <tkdgus3078@gmail.com>
2017-07-18 19:26:58 +0900

    매우 오랫만에 올리는 Git 어플 기능 상당수 구현

--- app/src/main/java/me/jihoon/bonae_android/Tab1Fragment.java
+++ app/src/main/java/me/jihoon/bonae_android/Tab1Fragment.java
@@ -7,16 +7,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by q on 2017-07-13.
  */
 
 public class Tab1Fragment extends Fragment {
+
+    String Token = null;
+    String Facebook_Id = null;
+    String Facebook_Name = null;
+
+    public void Setting(LayoutInflater inflater, ViewGroup container) {
+        Token = this.getArguments().getString("Token");
+        Facebook_Id = this.getArguments().getString("Facebook_Id");
+        Facebook_Name = this.getArguments().getString("Facebook_Name");
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 //        super.onCreateView(inflater, container, savedInstanceState);
         CustomInfoAdapter.UserAdapter adapter = new CustomInfoAdapter.UserAdapter();
         View view = inflater.inflate(R.layout.fragment_tab1, container, false);
+
+        Setting(inflater, container);
 
         ListView yes_list = (ListView) view.findViewById(R.id.listView_tab1yes);
         adapter.addUser("TestUser1");
